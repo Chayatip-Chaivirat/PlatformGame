cca3d1ed48713ab95439f3b9755b45a52a80b9b2
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-11-27 17:16:05 +0100

    Create CollidableObject.cs

---
+++ PlatformGame/CollidableObject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace PlatformGame
+{
+    abstract class CollidableObject
+    {
+        protected Texture2D tex;
+        protected Vector2 pos;
+        protected Rectangle hitBoxLive;
+
+        public abstract void Update();
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+        public bool CollisionDetected(CollidableObject other)
+        {
+            return this.hitBoxLive.Intersects(other.hitBoxLive);
+        }
+
+        public virtual void CollisionHandler(List<CollidableObject> listName)
+        {
+            listName.Remove(this); //this refers to the object in the class this method is used in
+        }
+    }
+}
