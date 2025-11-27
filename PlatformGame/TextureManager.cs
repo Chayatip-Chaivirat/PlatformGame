a2ebb0367774c8f0381874bb5bb0fadc92fb7daa
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-11-27 15:03:43 +0100

    TextureManager and PlayerKeyReader classes

---
+++ PlatformGame/TextureManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlatformGame
+{
+    internal class TextureManager
+    {
+        public static Texture2D grassTex;
+        public static Texture2D allLinkTex;
+        public static Texture2D wallTex;
+
+        public static void Textures (ContentManager content)
+        {
+            grassTex = content.Load<Texture2D>("grass");
+            allLinkTex = content.Load<Texture2D>("Link_all");
+            wallTex = content.Load<Texture2D>("wall");
+        }
+    }
+}
