7843d39a604c89f5b15c0289b596d2f12d76ec20
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-12-04 13:02:04 +0100

    Update

--- PlatformGame/Enemy.cs
+++ PlatformGame/Enemy.cs
@@ -30,6 +30,17 @@ namespace PlatformGame
             baseAttack = 3;
         }
 
+        public void ChangeMovementCode()
+        {
+            int newMovementCode;
+            do
+            {
+                newMovementCode = random.Next(1, 3); //From 1 to 2
+            }
+            while (newMovementCode == movementCode);
+
+            movementCode = newMovementCode;
+        }
         public override void Update(GameTime gameTime)
         {
             if(Detected(player))
